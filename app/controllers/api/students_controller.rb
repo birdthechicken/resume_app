48e33d1c19bcf5122e17c84a03567e63ca991080
birdthechicken <sinangundogar61@gmail.com>
2019-06-18 15:08:28 -0500

    uncommented cors file

--- app/controllers/api/students_controller.rb
+++ app/controllers/api/students_controller.rb
@@ -9,4 +9,22 @@ class Api::StudentsController < ApplicationController
     @student = Student.find(params[:id])
     render 'show.json.jbuilder'
   end
+
+  def update
+    @student = Student.find(params[:id])
+
+    @student.first_name = params[:first_name] || @student.first_name
+    @student.last_name = params[:last_name] || @student.last_name
+    @student.email = params[:email] || @student.email
+    @student.phone_number = params[:phone_number] || @student.phone_number
+    @student.short_bio = params[:short_bio] || @student.short_bio
+    @student.linkedIn_url = params[:linkedIn_url] || @student.linkedIn_url
+    @student.twitter_handle = params[:twitter_handle] || @student.twitter_handle
+    @student.personal_blog = params[:personal_blog] || @student.personal_blog
+    @student.website_url = params[:website_url] || @student.website_url
+    @student.resume_url = params[:resume_url] || @student.resume_url
+    @student.github_url = params[:github_url] || @student.github_url
+    @student.photo = params[:photo] || @student.photo
+
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,21 @@
 
 # student_5 = Student.create(first_name: "Sabrina", last_name: "DiMichele")
 # student_6 = Student.create(first_name: "Chris", last_name: "Bowen")
+
+
+
+
+
+test_student_10 = Student.create(first_name: "John", last_name: "Moose", email: "test@dot.com", phone_number: "773-555-5555", short_bio: "too short", linkedIn_url: "www.linkedin.com/johnmoose", twitter_handle: "twitter/johnmoose", personal_blog: "it is personal", website_url: "www.johnmoose.com", resume_url: "resume.com", github_url: "github/something", photo: "https://images.unsplash.com/photo-1516934024742-b461fba47600?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80")
+
+test_skill = Skill.create(student_id: test_student_10.id, skill_name: "so skill")
+test_skill = Skill.create(student_id: test_student_10.id, skill_name: "karate")
+test_skill = Skill.create(student_id: test_student_10.id, skill_name: "coding")
+
+test_experience = Experience.create(student_id: test_student_10.id, start_date: "1989-10-10", end_date: "2005-10-10", job_title: "office boy", company_name: "Apple", details: "no details yet")
+test_experience = Experience.create(student_id: test_student_10.id, start_date: "1099-10-10", end_date: "2001-10-10", job_title: "owner", company_name: "Actualize", details: "no details yet")
+test_experience = Experience.create(student_id: test_student_10.id, start_date: "2009-10-10", end_date: "2005-10-10", job_title: "battery charger", company_name: "AAA", details: "no details yet")
+
+test_education = Education.create(student_id: test_student_10.id, start_date: "2001-05-05", end_date: "2005-05-05", degree: "bachelor", schooling: "Actualize", details: "no details")
+
+test_capstone = Capstone.create(student_id: test_student_10.id, name: "Capstone name", description: "blablalba", url: "capstone.com", screenshot: "https://images.unsplash.com/photo-1516934024742-b461fba47600?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80")
